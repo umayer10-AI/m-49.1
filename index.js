1cf9fcc16802beb9e908435450c6c60cc4633faf
umayer10-AI <mdumayer10@gmail.com>
2026-05-04 19:17:46 +0600

    add

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/', (req,res) => {
-    res.send("hello from users")
+    res.send("hello from users Umayer")
 })
 
 app.get('/about', (req,res) => {
     res.send("Umayer Walking Now")
+})
+
+app.get('/product', (req,res) => {
+    res.send("Products are Now")
 })
 
 app.listen(port, () => {
