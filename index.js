b06845defeed4a29a88bf037aa3bd0a0adf38337
umayer10-AI <mdumayer10@gmail.com>
2026-05-04 20:46:52 +0600

    add

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.send("hello from users Umayer")
@@ -16,14 +20,21 @@ app.get('/about', (req,res) => {
     res.send(users)
 })
 
+app.post('/about', (req,res) => {
+    console.log("post Korsi" ,req.body)
+
+    const newUser = req.body
+    newUser.id = users.length+1
+    users.push(newUser)
+
+    res.send({success: true, data: newUser, message: 'Post Method is working'})
+})
+
 app.get('/product', (req,res) => {
     res.send("Products are Now")
 })
 
-app.post('/about', (res,req) => {
-    console.log("post Korsi")
-    res.send("Post Method is working")
-})
+
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
